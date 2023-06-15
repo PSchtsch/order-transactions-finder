ee289f3473e1f37be62b9a7f2f60f66418d60e25
PSchtsch <pavelshh.s@gmail.com>
2023-06-15 16:19:37 +0300

    add project implementation

---
+++ TransactionFinderForm.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Forms;
+using TransactionsFinder.Controllers;
+
+namespace TransactionsFinder
+{
+    public partial class TransactionsFinderForm : Form
+    {
+        private readonly ITransactionController _transactionController = new TransactionController();
+
+        public TransactionsFinderForm()
+        {
+            InitializeComponent();
+        }
+
+        private void searchBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Enter) return;
+
+            var entryNumber = searchBox.Text;
+            dataGridView.DataSource = _transactionController.GetAllRelatedTransactions(entryNumber);
+        }
+    }
+}
